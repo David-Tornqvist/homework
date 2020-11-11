3b5d9a511a6cf937d9eed6f2db1c81ffc62dbaaa
Dmannen03 <39675750+Dmannen03@users.noreply.github.com>
2020-11-11 18:06:23 +0100

    klar

---
+++ 46/3/script.js
@@ -0,0 +1,15 @@
+let animal = {
+    species: "Cat",
+    age: 4,
+    name: "Dante"
+};
+
+console.log(animal);
+
+animal.age ++;
+
+console.log(animal);
+
+animal.age += 1;
+
+console.log(animal);
