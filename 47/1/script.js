d39ab98f87982df53ba10499a0b8a2aef9270dda
Dmannen03 <david.tornqvist@botkyrka.skola.se>
2020-11-23 18:05:40 +0100

    Nåt gick fel med github

--- 47/1/script.js
+++ 47/1/script.js
@@ -1,8 +1,9 @@
 
-// låt i vara 0
+let i = 0;
 
-// while i < 6
-//      logga ut i
-//      öka i med 1
-
-// logga ut "Färdig!"
+while (i < 6){
+    console.log(i);
+    i++
+}
+      
+console.log("Färdig")
